bf68d3437c0ccafc1b945b9821fc9538fa305e45
NathanMars <nathan.manoraj@gmail.com>
2026-05-25 10:57:40 +0900

    Project 4: incremental model

---
+++ models/marts/mart_taxi_trips_daily_incremental.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='incremental',
+    unique_key='trip_date'
+) }}
+
+with trips as (
+
+    select * from {{ ref('stg_taxi_trips') }}
+
+    {% if is_incremental() %}
+        where trip_date > (select max(trip_date) from {{ this }})
+    {% endif %}
+
+),
+
+final as (
+
+    select
+        trip_date,
+        count(taxi_id)              as total_trips,
+        round(avg(fare), 2)         as avg_fare,
+        round(avg(tips), 2)         as avg_tip,
+        round(sum(trip_total), 2)   as total_revenue
+    from trips
+    group by trip_date
+    order by trip_date
+
+)
+
+select * from final
